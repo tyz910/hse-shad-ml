e62f33d1b0d10e9ea635ae7f513851d940de17ea
Evgenii Ivanov <tyz910@gmail.com>
2019-04-10 17:15:55 +0300

    03-statement-neighbours

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ def print_answer(num: int, text: str) -> None:
     if not os.path.exists("answers"):
         os.makedirs("answers")
 
-    with open(f"answers/a{num}.txt", "w") as f:
+    with open(os.path.join("answers", f"a{num}.txt"), "w") as f:
         f.write(text)
 
     print(text)
